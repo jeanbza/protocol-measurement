5fa17a4b1ad31581af31855143f15b5e5c45eff8
Jean de Klerk <deklerk@google.com>
2018-01-09 10:19:04 -0800

    Fix k8s udp config

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,7 @@ QUIC_SENDER_PORT=8076
 function deploy {
     NAME=$1
     PORT=$2
+    PROTOCOL=$3
 
     GOOS=linux go build .
 
@@ -33,7 +34,7 @@ function deploy {
     docker build --no-cache -t gcr.io/${PROJECT_ID}/$NAME:$VERSION .
     gcloud docker -- push gcr.io/${PROJECT_ID}/$NAME:$VERSION
     kubectl run $NAME --image=gcr.io/${PROJECT_ID}/$NAME:$VERSION --port 8080
-    kubectl expose deployment $NAME --type=LoadBalancer --port $PORT --target-port 8080
+    kubectl expose deployment $NAME --type=LoadBalancer --port=$PORT --target-port=8080 --protocol=$PROTOCOL
 
     for attempt in $( eval echo {0..$MAX_IP_ATTEMPTS} ); do
         echo "Fetch IP attempt $attempt / $MAX_IP_ATTEMPTS"
@@ -56,71 +57,72 @@ gcloud container clusters get-credentials b-node-cluster --zone us-central1-a --
 pushd orchestrator
     npm install
     ./node_modules/.bin/webpack --progress
-    deploy orchestrator $ORCHESTRATOR_PORT
+    deploy orchestrator $ORCHESTRATOR_PORT TCP
     rm orchestrator
 popd
 
 pushd receivers/http
-    deploy http-receiver $HTTP_RECEIVER_PORT
+    deploy http-receiver $HTTP_RECEIVER_PORT TCP
     HTTP_RECEIVER_IP=$IP
     rm http
 popd
 
 pushd receivers/udp
-    deploy udp-receiver $UDP_RECEIVER_PORT
+    deploy udp-receiver $UDP_RECEIVER_PORT UDP
     UDP_RECEIVER_IP=$IP
     rm udp
 popd
 
 pushd receivers/unary_grpc
-    deploy unary-grpc-receiver $UNARY_GRPC_RECEIVER_PORT
+    deploy unary-grpc-receiver $UNARY_GRPC_RECEIVER_PORT TCP
     UNARY_GRPC_RECEIVER_IP=$IP
     rm unary_grpc
 popd
 
 pushd receivers/streaming_grpc
-    deploy streaming-grpc-receiver $STREAMING_GRPC_RECEIVER_PORT
+    deploy streaming-grpc-receiver $STREAMING_GRPC_RECEIVER_PORT TCP
     STREAMING_GRPC_RECEIVER_IP=$IP
     rm streaming_grpc
 popd
 
 pushd receivers/streaming_websocket
-    deploy streaming-websocket-receiver $STREAMING_WEBSOCKET_RECEIVER_PORT
+    deploy streaming-websocket-receiver $STREAMING_WEBSOCKET_RECEIVER_PORT TCP
     STREAMING_WEBSOCKET_RECEIVER_IP=$IP
     rm streaming_websocket
 popd
 
+# TODO: these don't need to be exposed - maybe add a flag to ignore?
 gcloud container clusters get-credentials c-node-cluster --zone asia-south1-c --project deklerk-sandbox
 pushd senders/http
-    deploy http-sender $HTTP_SENDER_PORT
+    deploy http-sender $HTTP_SENDER_PORT TCP
     kubectl set env deployments/http-sender HTTP_RECEIVER_IP=$HTTP_RECEIVER_IP
     kubectl set env deployments/http-sender HTTP_RECEIVER_PORT=$HTTP_RECEIVER_PORT
     rm http
 popd
 
 pushd senders/udp
-    deploy udp-sender $UDP_SENDER_PORT
+    deploy udp-sender $UDP_SENDER_PORT TCP
     kubectl set env deployments/udp-sender UDP_RECEIVER_IP=$UDP_RECEIVER_IP
     kubectl set env deployments/udp-sender UDP_RECEIVER_PORT=$UDP_RECEIVER_PORT
     rm udp
 popd
 
 pushd senders/unary_grpc
-    deploy unary-grpc-sender $UNARY_GRPC_SENDER_PORT
+    deploy unary-grpc-sender $UNARY_GRPC_SENDER_PORT TCP
     kubectl set env deployments/unary-grpc-sender UNARY_GRPC_RECEIVER_IP=$UNARY_GRPC_RECEIVER_IP
     kubectl set env deployments/unary-grpc-sender UNARY_GRPC_RECEIVER_PORT=$UNARY_GRPC_RECEIVER_PORT
     rm unary_grpc
 popd
 
 pushd senders/streaming_grpc
-    deploy streaming-grpc-sender $STREAMING_GRPC_SENDER_PORT
+    deploy streaming-grpc-sender $STREAMING_GRPC_SENDER_PORT TCP
     kubectl set env deployments/streaming-grpc-sender STREAMING_GRPC_RECEIVER_IP=$STREAMING_GRPC_RECEIVER_IP
     kubectl set env deployments/streaming-grpc-sender STREAMING_GRPC_RECEIVER_PORT=$STREAMING_GRPC_RECEIVER_PORT
     rm streaming_grpc
 popd
 
 pushd senders/streaming_websocket
-    deploy streaming-websocket-sender $STREAMING_WEBSOCKET_SENDER_PORT
+    deploy streaming-websocket-sender $STREAMING_WEBSOCKET_SENDER_PORT TCP
     kubectl set env deployments/streaming-websocket-sender STREAMING_WEBSOCKET_RECEIVER_IP=$STREAMING_WEBSOCKET_RECEIVER_IP
     kubectl set env deployments/streaming-websocket-sender STREAMING_WEBSOCKET_RECEIVER_PORT=$STREAMING_WEBSOCKET_RECEIVER_PORT
     rm streaming_websocket
